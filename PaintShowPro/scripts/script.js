2e48e6a0ef0cfa82646d99d2e79c65c35bb6252b
TodorVyagov <todor.vyagov@gmail.com>
2014-06-06 14:50:45 +0300

    Linked JavaScript file with HTML and a little test function

--- PaintShowPro/scripts/script.js
+++ PaintShowPro/scripts/script.js
@@ -1,1 +1,5 @@
-﻿// my first commit 
+﻿// changed background color just for testing
+window.onload = function () {
+    document.body.style.backgroundColor = 'rgba(231, 142, 218, 1)';
+}
+// end of testing - Can be deleted

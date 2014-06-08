c51c68d3d9bef8e9683575729ca53f329ceb7440
Gabriela Angelova <gabriela.angelova@fdiba.tu-sofia.bg>
2014-06-08 16:20:41 +0300

    refactored file script.js + elipse function as new script

--- PaintShowPro/scripts/script.js
+++ PaintShowPro/scripts/script.js
@@ -8,85 +8,6 @@
     var mainLayer = new Kinetic.Layer();
     stage.add(mainLayer);
 
-    ///draw resizable circle
-    //var circleButton = document.getElementsByClassName('circle')[0];
-    //circleButton.addEventListener('click', function () { drawResizableCircle() });
-    var canvasOffset = $("#canvas-container").offset();
-    var offsetX = canvasOffset.left;
-    var offsetY = canvasOffset.top;
-    var startX;
-    var startY;
-    var isDown = false;
-
-    function drawResizableCircle() {
-
-        $("#canvas-container").mousedown(function (e) {
-            handleMouseDown(e);
-        });
-        $("#canvas-container").mousemove(function (e) {
-            handleMouseMove(e);
-        });
-        $("#canvas-container").mouseup(function (e) {
-            handleMouseUp(e);
-        });
-        $("#canvas-container").mouseout(function (e) {
-            handleMouseOut(e);
-        });
-    }
-
-    function handleMouseDown(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        startX = parseInt(e.clientX - offsetX);
-        startY = parseInt(e.clientY - offsetY);
-        isDown = true;
-    }
-
-    function handleMouseUp(e) {
-        if (!isDown) {
-            return;
-        }
-        e.preventDefault();
-        e.stopPropagation();
-        isDown = false;
-    }
-
-    function handleMouseOut(e) {
-        if (!isDown) {
-            return;
-        }
-        e.preventDefault();
-        e.stopPropagation();
-        isDown = false;
-    }
-
-    function handleMouseMove(e) {
-        if (!isDown) {
-            return;
-        }
-        e.preventDefault();
-        e.stopPropagation();
-        mouseX = parseInt(e.clientX - offsetX);
-        mouseY = parseInt(e.clientY - offsetY);
-        drawOval(mouseX, mouseY);
-    }
-
-    function drawOval(x, y) {
-        var oval = new Kinetic.Shape({
-            sceneFunc: function (context) {
-                context.clearRect(0, 0, 900, 600);
-                context.beginPath();
-                context.moveTo(startX, startY + (y - startY) / 2);
-                context.bezierCurveTo(startX, startY, x, startY, x, startY + (y - startY) / 2);
-                context.bezierCurveTo(x, y, startX, y, startX, startY + (y - startY) / 2);
-                context.closePath();
-                context.stroke();
-            }
-        });
-        mainLayer.add(oval);
-        stage.add(mainLayer);
-    }
-
 
     ///////////////// pen - draw free line
 
@@ -98,6 +19,9 @@
 
     var circleButton = document.getElementById('circle');
     circleButton.addEventListener('click', function () { drawCircle(stage, mainLayer) });
+
+    var elipseButton = document.getElementById('elipse');
+    elipseButton.addEventListener('click', function () { drawElipse(stage,mainLayer) });
 
     function drawPen() {
         var isMouseDown = false;

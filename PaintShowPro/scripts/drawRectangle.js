b7c15e23dcd746a26d3d3872f91472e8d44fe191
wnvko <milko.venkov@gmail.com>
2014-06-08 11:40:35 +0300

    working on rectangle

---
+++ PaintShowPro/scripts/drawRectangle.js
@@ -0,0 +1,21 @@
+﻿function drawRectangle(stage) {
+    var isMouseDown = false,
+        mouseClickX,
+        mouseClickY,
+        rectStroke = document.getElementById('strokeColor').value,
+        rectStrokeWidth = document.getElementById('strokeWeight').value,
+        rectFill = document.getElementById('fillColor').value,
+        rectLayer = new Kinetic.Layer;
+
+
+    var dynamicRectangle = new Kinetic.Rect({
+        width: 100,
+        height: 100,
+        fill: rectFill,
+        stroke: rectStroke,
+        strokeWidth: rectStrokeWidth
+    });
+
+    rectLayer.add(dynamicRectangle);
+    stage.add(rectLayer);
+}

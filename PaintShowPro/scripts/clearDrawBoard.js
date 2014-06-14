07ccc0841a784358062f4474b4ce436413456467
wnvko <milko.venkov@gmail.com>
2014-06-14 10:56:07 +0300

    Clear draw board function added

---
+++ PaintShowPro/scripts/clearDrawBoard.js
@@ -0,0 +1,9 @@
+﻿function clearDrawBoard(e) {
+    var stage = e.target.stage,
+        mainLayer = e.target.mainLayer;
+
+    mainLayer.destroy();
+
+    var mainLayer = new Kinetic.Layer();
+    stage.add(mainLayer);
+}

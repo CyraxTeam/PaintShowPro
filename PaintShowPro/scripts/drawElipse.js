20eb8566605e610cc484251a4eb035cffba61add
wnvko <milko.venkov@gmail.com>
2014-06-14 11:32:32 +0300

    Ellipse fixed

--- PaintShowPro/scripts/drawElipse.js
+++ PaintShowPro/scripts/drawElipse.js
@@ -1,80 +1,79 @@
 ﻿function drawElipse(e) {
-    var canvasOffset = $("#canvas-container").offset();
-    var offsetX = canvasOffset.left;
-    var offsetY = canvasOffset.top;
-    var startX;
-    var startY;
-    var isDown = false;
-    var stage = e.target.stage;
-    var mainLayer = e.target.mainLayer;
+    var isMouseDown = false,
+        stage = e.target.stage,
+        mainLayer = e.target.mainLayer,
+        startX,
+        startY,
+        canvasOffset = $("#canvas-container").offset(),
+        offsetX = canvasOffset.left,
+        offsetY = canvasOffset.top,
+        dynamicElipse,
+        elipseStroke = document.getElementById('strokeColor').value,
+        elipseStrokeWidth = document.getElementById('strokeWeight').value,
+        elipseFill = document.getElementById('fillColor').value,
+        elipseLayer = new Kinetic.Layer;
 
 
     $("#canvas-container").mousedown(function (e) {
         handleMouseDown(e);
     });
+
     $("#canvas-container").mousemove(function (e) {
         handleMouseMove(e);
     });
+
     $("#canvas-container").mouseup(function (e) {
         handleMouseUp(e);
-    });
-    $("#canvas-container").mouseout(function (e) {
-        handleMouseOut(e);
     });
 
 
     function handleMouseDown(e) {
         e.preventDefault();
         e.stopPropagation();
-        startX = parseInt(e.clientX - offsetX);
-        startY = parseInt(e.clientY - offsetY);
-        isDown = true;
-    }
-
-    function handleMouseUp(e) {
-        if (!isDown) {
-            return;
-        }
-        e.preventDefault();
-        e.stopPropagation();
-        $("#canvas-container").unbind();
-        isDown = false;
-    }
-
-    function handleMouseOut(e) {
-        if (!isDown) {
-            return;
-        }
-        e.preventDefault();
-        e.stopPropagation();
-        $("#canvas-container").unbind();
-        isDown = false;
+        var mousePos = stage.getPointerPosition();
+        startX = mousePos.x;
+        startY = mousePos.y;
+        isMouseDown = true;
     }
 
     function handleMouseMove(e) {
-        if (!isDown) {
+        if (!isMouseDown) {
             return;
         }
+
+        elipseLayer.removeChildren();
         e.preventDefault();
         e.stopPropagation();
-        mouseX = parseInt(e.clientX - offsetX);
-        mouseY = parseInt(e.clientY - offsetY);
-        drawOval(mouseX, mouseY);
+        var mousePos = stage.getPointerPosition();
+        mouseX = mousePos.x;
+        mouseY = mousePos.y;
+        drawDynamicElipse();
+        elipseLayer.add(dynamicElipse);
+        stage.add(elipseLayer);
     }
 
-    function drawOval(x, y) {
-        var oval = new Kinetic.Shape({
-            sceneFunc: function (context) {
-                context.clearRect(0, 0, 900, 600);
-                context.beginPath();
-                context.moveTo(startX, startY + (y - startY) / 2);
-                context.bezierCurveTo(startX, startY, x, startY, x, startY + (y - startY) / 2);
-                context.bezierCurveTo(x, y, startX, y, startX, startY + (y - startY) / 2);
-                context.closePath();
-                context.stroke();
-            }
-        });
-        mainLayer.add(oval);
+    function handleMouseUp(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        mainLayer.add(dynamicElipse)
         stage.add(mainLayer);
+        elipseLayer.destroy();
+        $("#canvas-container").unbind();
+        isMouseDown = false;
+    }
+
+
+    function drawDynamicElipse() {
+
+        dynamicElipse = new Kinetic.Ellipse({
+            x: startX,
+            y: startY,
+            width: Math.abs(mouseX - startX) * 2,
+            height: Math.abs(mouseY - startY) * 2,
+            fill: document.getElementById('fillColor').value,
+            stroke: document.getElementById('strokeColor').value,
+            strokeWidth: document.getElementById('strokeWeight').value,
+            draggable: true
+        });
     }
 }
